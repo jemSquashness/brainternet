48ce1fd4a7426a5366e7e4c9b01fd98b2203b2be
DanielleJoyWinter <563795@students.wits.ac.za>
2016-10-26 13:03:17 +0200

    pickle files and changes to prediction.py added

--- prediction.py
+++ prediction.py
@@ -7,17 +7,18 @@ import numpy as np
 from sklearn.svm import SVC
 from sklearn.externals import joblib
 from scipy import signal
+import pickle
 
-F3_classifier = joblib.load('F3_classifier.pkl')
-AF3_classifier = joblib.load('AF3_classifier.pkl')
-F4_classifier = joblib.load('F4_classifier.pkl')
-AF4_classifier = joblib.load('F4_classifier.pkl')
+
+F3_classifier = pickle.load(open('F3_classifier.pickle'))
+AF3_classifier = pickle.load(open('AF3_classifier.pickle'))
+F4_classifier = pickle.load(open('F4_classifier.pickle'))
+AF4_classifier = pickle.load(open('F4_classifier.pickle'))
 
 
 def prediction_on_new_data(classifier, new_data):
     predicted_input = classifier.predict(new_data)
     print predicted_input
-
 
 def high_pass_filter(data):
     b, a = signal.butter(5, 0.16, 'high', analog=False, output='ba')
@@ -28,12 +29,12 @@ def high_pass_filter(data):
 # Read in new data
 
 # Filter new data
-F3_filt = high_pass_filter(F3_4s_data)
-AF3_filt = high_pass_filter(AF3_4s_data)
-F4_filt = high_pass_filter(F4_4s_data)
-AF4_filt = high_pass_filter(AF4_4s_data)
-
-F3_predicted_input = prediction_on_new_data(F3_classifier, F3_filt)
-AF3_predicted_input = prediction_on_new_data(AF3_classifier, AF3_filt)
-F4_predicted_input = prediction_on_new_data(F4_classifier, F4_filt)
-AF4_predicted_input = prediction_on_new_data(AF4_classifier, AF4_filt)
+# F3_filt = high_pass_filter(F3_4s_data)
+# AF3_filt = high_pass_filter(AF3_4s_data)
+# F4_filt = high_pass_filter(F4_4s_data)
+# AF4_filt = high_pass_filter(AF4_4s_data)
+#
+# F3_predicted_input = prediction_on_new_data(F3_classifier, F3_filt)
+# AF3_predicted_input = prediction_on_new_data(AF3_classifier, AF3_filt)
+# F4_predicted_input = prediction_on_new_data(F4_classifier, F4_filt)
+# AF4_predicted_input = prediction_on_new_data(AF4_classifier, AF4_filt)
